3f2d7e9da6ad0e8d850be05c5fdd4120cd4a5a78
Anthony Do <anthonyweido@gmail.com>
2021-06-10 07:16:19 +0000

    Properly removed the Authentication and routing to be put into two different files

---
+++ routes.js
@@ -0,0 +1,63 @@
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
+module.exports = function (app,myDataBase){
+
+  
+function ensureAuthenticated(req,res,next){
+  if(req.isAuthenticated()) return next();
+  res.redirect('/');
+}
+
+   //ROUTES AND RENDERS
+    app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true,
+      showRegistration: true
+    });
+  });
+
+  app.route("/login").post(passport.authenticate('local', {failureRedirect: '/'}), (req,res) => {
+    res.render('/profile');
+  });
+
+  app.route('/profile').get(ensureAuthenticated, (req,res) => {
+    res.render(process.cwd() + "/views/pug/profile", {username: req.user.username});
+  });
+
+  app.route('/logout').get((req,res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  //So this effects the path under "register"
+  app.route('/register').post((req,res, next) => {//So after registering we want to find a single user
+    const hash = bcrypt.hashSync(req.body.password, 12);
+    myDataBase.findOne({username: req.body.username}, function(err,user) {
+      if(err){//If there's an error then we'll error out
+        next(err);
+      } else if(user) { //If the user already exists we'll just go back to home
+        res.redirect('/');
+      }else //Otherwise we'll insert a new object with Username and Password
+      {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: hash
+        }, (err,doc) => {
+          if(err){//If it can't do it then we reroute out
+            res.redirect('/');
+          }else{//if we do succeed we go to the next activity
+            next(null, doc.ops[0]);
+          }
+        }
+        )
+      }
+    })//Then we authenticate 
+  }, passport.authenticate('local', {failureRedirect: '/'}), 
+        (req,res,next) => {res.redirect('/profile');}
+  );
+
+}
--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const app = express();
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
+//const passport = require('passport');
 const session = require('express-session');
-const passport = require('passport');
+//const LocalStrategy = require('passport-local');
 const ObjectID = require('mongodb').ObjectID;
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
-function ensureAuthenticated(req,res,next){
-  if(req.isAuthenticated()) return next();
-  res.redirect('/');
-}
 
 app.set('view engine', 'pug');
 fccTesting(app); //For FCC testing purposes
@@ -31,92 +29,15 @@ app.use(session({
   cookie: { secure: false }
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 //Start of DB
 
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
-  
-  //ROUTES AND RENDERS
-    app.route('/').get((req, res) => {
-    // Change the response to render the Pug template
-    res.render('pug', {
-      title: 'Connected to Database',
-      message: 'Please login',
-      showLogin: true,
-      showRegistration: true
-    });
-  });
-
-  app.route("/login").post(passport.authenticate('local', {failureRedirect: '/'}), (req,res) => {
-    res.render('/profile');
-  });
-
-  app.route('/profile').get(ensureAuthenticated, (req,res) => {
-    res.render(process.cwd() + "/views/pug/profile", {username: req.user.username});
-  });
-
-  app.route('/logout').get((req,res) => {
-    req.logout();
-    res.redirect("/");
-  });
-
-  //So this effects the path under "register"
-  app.route('/register').post((req,res, next) => {//So after registering we want to find a single user
-    const hash = bcrypt.hashSync(req.body.password, 12);
-    myDataBase.findOne({username: req.body.username}, function(err,user) {
-      if(err){//If there's an error then we'll error out
-        next(err);
-      } else if(user) { //If the user already exists we'll just go back to home
-        res.redirect('/');
-      }else //Otherwise we'll insert a new object with Username and Password
-      {
-        myDataBase.insertOne({
-          username: req.body.username,
-          password: hash
-        }, (err,doc) => {
-          if(err){//If it can't do it then we reroute out
-            res.redirect('/');
-          }else{//if we do succeed we go to the next activity
-            next(null, doc.ops[0]);
-          }
-        }
-        )
-      }
-    })//Then we authenticate 
-  }, passport.authenticate('local', {failureRedirect: '/'}), 
-        (req,res,next) => {res.redirect('/profile');}
-  );
-
-  app.use((req,res,next) => {
-      res.status(404)
-        .type("text")
-        .send("not found");
-  });
-
-  // Serialization and deserialization here...
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-      myDataBase.findOne({ username: username }, function (err, user) {
-        console.log('User '+ username +' attempted to log in.');
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        //if (password !== user.password) { return done(null, false); }
-        if(!bcrypt.compareSync(password, user.password)){return done(null, false);}
-        return done(null, user);
-      });
-    }
-  ));
+  routes(app, myDataBase);
+  auth(app,myDataBase);
   // Be sure to add this...
 }).catch((e) => {
   app.route('/').get((req, res) => {
